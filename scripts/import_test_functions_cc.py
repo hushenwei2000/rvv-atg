234923dbd671d63804429cf120461d0c414c076b
Chuanning Wang <wangchuan_n@163.comWcn990923!git config --global user.email wangchuan_n@163.com>
2022-09-20 19:58:22 +0800

    test int fix load

---
+++ scripts/import_test_functions_cc.py
@@ -0,0 +1,154 @@
+# integer
+from scripts.create_test_integer.create_test_vand import *
+from scripts.create_test_integer.create_test_vadd import *
+from scripts.create_test_integer.create_test_vadc import *
+from scripts.create_test_integer.create_test_vdiv import *
+from scripts.create_test_integer.create_test_vdivu import *
+from scripts.create_test_integer.create_test_vmacc import *
+from scripts.create_test_integer.create_test_vmadc import *
+from scripts.create_test_integer.create_test_vmsbc import *
+from scripts.create_test_integer.create_test_vmadd import *
+from scripts.create_test_integer.create_test_vmax import *
+from scripts.create_test_integer.create_test_vmaxu import *
+from scripts.create_test_integer.create_test_vmin import *
+from scripts.create_test_integer.create_test_vminu import *
+from scripts.create_test_integer.create_test_vmseq import *
+from scripts.create_test_integer.create_test_vmsgt import *
+from scripts.create_test_integer.create_test_vmsgtu import *
+from scripts.create_test_integer.create_test_vmsle import *
+from scripts.create_test_integer.create_test_vmsleu import *
+from scripts.create_test_integer.create_test_vmslt import *
+from scripts.create_test_integer.create_test_vmsltu import *
+from scripts.create_test_integer.create_test_vmsne import *
+from scripts.create_test_integer.create_test_vmul import *
+from scripts.create_test_integer.create_test_vmulh import *
+from scripts.create_test_integer.create_test_vmulhu import *
+from scripts.create_test_integer.create_test_vmulhsu import *
+from scripts.create_test_integer.create_test_vnmsac import *
+from scripts.create_test_integer.create_test_vnmsub import *
+from scripts.create_test_integer.create_test_vnsra import *
+from scripts.create_test_integer.create_test_vnsrl import *
+from scripts.create_test_integer.create_test_vor import *
+from scripts.create_test_integer.create_test_vredand import *
+from scripts.create_test_integer.create_test_vredmax import *
+from scripts.create_test_integer.create_test_vredmaxu import *
+from scripts.create_test_integer.create_test_vredmin import *
+from scripts.create_test_integer.create_test_vredminu import *
+from scripts.create_test_integer.create_test_vredor import *
+from scripts.create_test_integer.create_test_vredsum import *
+from scripts.create_test_integer.create_test_vredxor import *
+from scripts.create_test_integer.create_test_vrem import *
+from scripts.create_test_integer.create_test_vremu import *
+from scripts.create_test_integer.create_test_vrgather import *
+from scripts.create_test_integer.create_test_vrsub import *
+from scripts.create_test_integer.create_test_vsadd import *
+from scripts.create_test_integer.create_test_vsaddu import *
+from scripts.create_test_integer.create_test_vsbc import *
+from scripts.create_test_integer.create_test_vsll import *
+from scripts.create_test_integer.create_test_vsra import *
+from scripts.create_test_integer.create_test_vsrl import *
+from scripts.create_test_integer.create_test_vssub import *
+from scripts.create_test_integer.create_test_vssubu import *
+from scripts.create_test_integer.create_test_vsub import *
+from scripts.create_test_integer.create_test_vxor import *
+from scripts.create_test_integer.create_test_vwadd import *
+from scripts.create_test_integer.create_test_vwaddu import *
+from scripts.create_test_integer.create_test_vwmacc import *
+from scripts.create_test_integer.create_test_vwmaccu import *
+from scripts.create_test_integer.create_test_vwmaccus import *
+from scripts.create_test_integer.create_test_vwmaccsu import *
+from scripts.create_test_integer.create_test_vwredsum import *
+from scripts.create_test_integer.create_test_vwredsumu import *
+from scripts.create_test_integer.create_test_vwmul import *
+from scripts.create_test_integer.create_test_vwmulu import *
+from scripts.create_test_integer.create_test_vwmulsu import *
+from scripts.create_test_integer.create_test_vwsub import *
+from scripts.create_test_integer.create_test_vwsubu import *
+# fix-point
+from scripts.create_test_fixpoint.create_test_vaadd import *
+from scripts.create_test_fixpoint.create_test_vaaddu import *
+from scripts.create_test_fixpoint.create_test_vasubu import *
+from scripts.create_test_fixpoint.create_test_vasub import *
+from scripts.create_test_fixpoint.create_test_vnclip import *
+from scripts.create_test_fixpoint.create_test_vnclipu import *
+from scripts.create_test_fixpoint.create_test_vsmul import *
+from scripts.create_test_fixpoint.create_test_vssra import *
+from scripts.create_test_fixpoint.create_test_vssrl import *
+# load 
+from scripts.create_test_loadstore.create_test_vle8 import *
+from scripts.create_test_loadstore.create_test_vle16 import *
+from scripts.create_test_loadstore.create_test_vle32 import *
+from scripts.create_test_loadstore.create_test_vle64 import *
+from scripts.create_test_loadstore.create_test_vlse8 import *
+from scripts.create_test_loadstore.create_test_vlse16 import *
+from scripts.create_test_loadstore.create_test_vlse32 import *
+from scripts.create_test_loadstore.create_test_vlse64 import *
+from scripts.create_test_loadstore.create_test_vlre8 import *
+from scripts.create_test_loadstore.create_test_vlre16 import *
+from scripts.create_test_loadstore.create_test_vlre32 import *
+from scripts.create_test_loadstore.create_test_vlsege8 import *
+from scripts.create_test_loadstore.create_test_vlsege16 import *
+from scripts.create_test_loadstore.create_test_vlsege32 import *
+from scripts.create_test_loadstore.create_test_vlssege8 import *
+from scripts.create_test_loadstore.create_test_vlssege16 import *
+from scripts.create_test_loadstore.create_test_vlssege32 import *
+from scripts.create_test_loadstore.create_test_vluxei8 import *
+from scripts.create_test_loadstore.create_test_vluxei16 import *
+from scripts.create_test_loadstore.create_test_vluxei32 import *
+from scripts.create_test_loadstore.create_test_vluxsegei8 import *
+from scripts.create_test_loadstore.create_test_vluxsegei16 import *
+from scripts.create_test_loadstore.create_test_vluxsegei32 import *
+#store
+from scripts.create_test_loadstore.create_test_vse8 import *
+from scripts.create_test_loadstore.create_test_vse16 import *
+from scripts.create_test_loadstore.create_test_vse32 import *
+from scripts.create_test_loadstore.create_test_vs1r import *
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# floating point
+from scripts.create_test_floating.create_test_vfmv import *
+
+# mask
+from scripts.create_test_mask.create_test_vmand import *
+
+
+# permute
