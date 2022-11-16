74531bff450c0ebd5cf3f78022c1577f2392cdda
Shenwei HU <shenwei.h@rioslab.org>
2022-11-16 22:13:51 +0800

    Add fix-point test all elements (finfish)

--- scripts/create_test_fixpoint/create_test_vnclip.py
+++ scripts/create_test_fixpoint/create_test_vnclip.py
@@ -6,46 +6,6 @@ import re
 
 instr = 'vnclip'
 
-
-def generate_tests(f, rs1_val, rs2_val):
-    n = 1
-    print("  #-------------------------------------------------------------", file=f)
-    print("  # WV Tests", file=f)
-    print("  #-------------------------------------------------------------", file=f)
-    print("  RVTEST_SIGBASE( x12,signature_x12_1)", file=f)
-    for i in range(len(rs1_val)):
-        n += 1
-        print("  TEST_W_AVG_WV_OP( "+str(n)+",  %s.wv, " %
-              instr+"5201314"+", "+"5201314"+", "+"5201314"+", "+"5201314"+", "+rs2_val[i]+", "+rs1_val[i]+" );", file=f)
-    for i in range(100):     
-        k = i%31+1
-        n+=1
-        print("  TEST_W_AVG_WV_OP_rd%d( "%k+str(n)+",  %s.wv, "%instr+"5201314"+", "+"5201314"+", "+"5201314"+", "+"5201314"+", "+rs2_val[i]+", "+rs1_val[i]+");",file=f)
-        
-        k = i%30+2
-        # if(k==14):
-        #     continue;
-        n +=1
-        print("  TEST_W_AVG_WV_OP_1%d( "%k+str(n)+",  %s.wv, "%instr+"5201314"+", "+"5201314"+", "+"5201314"+", "+"5201314"+", "+rs2_val[i]+", "+rs1_val[i]+" );",file=f)
-    print("  #-------------------------------------------------------------", file=f)
-    print("  # WX Tests", file=f)
-    print("  #-------------------------------------------------------------", file=f)
-    print("  RVTEST_SIGBASE( x20,signature_x20_1)", file=f)
-    for i in range(len(rs1_val)):
-        n += 1
-        print("  TEST_W_AVG_WX_OP( "+str(n)+",  %s.wx, " %
-              instr+"5201314"+", "+"5201314"+", "+"5201314"+", "+"5201314"+", "+rs2_val[i]+", "+rs1_val[i]+" );", file=f)
-    print("  #-------------------------------------------------------------", file=f)
-    print("  # WI Tests", file=f)
-    print("  #-------------------------------------------------------------", file=f)
-    print("  RVTEST_SIGBASE( x20,signature_x20_1)", file=f)
-    for i in range(len(rs1_val)):
-        n += 1
-        print("  TEST_W_AVG_WI_OP( "+str(n)+",  %s.wi, " %
-              instr+"5201314"+", "+"5201314"+", "+"5201314"+", "+"5201314"+", "+rs2_val[i]+", "+" 4 "+" );", file=f)
-  
-
-
 def create_empty_test_vnclip(xlen, vlen, vsew, lmul, vta, vma, output_dir):
     logging.info("Creating empty test for {}".format(instr))
 
@@ -85,10 +45,10 @@ def create_first_test_vnclip(xlen, vlen, vsew, lmul, vta, vma, output_dir, rpt_p
     generate_macros_vnclip(f, lmul)
 
     # Generate tests
-    generate_tests_vnclip(f, rs1_val, rs2_val, instr, lmul)
+    num_tests_tuple = generate_tests_vnclip(f, rs1_val, rs2_val, instr, lmul)
 
     # Common const information
-    print_common_ending(f)
+    print_common_ending_rs1rs2rd_vvvxvi(rs1_val, rs2_val, num_tests_tuple, vsew, f, rs2_data_multiplier = 2)
 
     f.close()
     os.system("cp %s %s" % (path, output_dir))
