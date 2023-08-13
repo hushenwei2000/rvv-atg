e60ccf590cc9ee738f378a95f0c0628ccf028f34
Shenwei HU <shenwei.h@rioslab.org>
2023-08-13 14:15:58 +0800

    update generate_all

--- generate_all.py
+++ generate_all.py
@@ -8,7 +8,8 @@ from scripts.lib import setup_logging
 # IMPORTANT!
 # If you use `python3` not `python`, you should replace all `python` in this file to `python3`
 # Please Read this file roughly before running it.
-# It is recommended to not to run too many instructions at once, because it will take a long time to finish and may hard to debug when have bugs. 
+
+
 
 categories = ["integer", "mask", "floatingpoint", "permute", "fixpoint", "loadstore"]
 
@@ -37,25 +38,32 @@ all = dict(integer=integer, mask=mask, floatingpoint=floatingpoint, permute=perm
 
 # Modify here if you want to test different VSEW, VLEN, LMUL ect..
 def runcommand_integer(ins):
-    os.system('python run.py -t i -i %s --vlen 256 --vsew 16 --lmul 8' % ins)
-
+    if (vsew == 8 or vsew == 64 or lmul_str == "0.125" or lmul_str == "8") and (ins.startswith('vw') or ins.startswith('vn')):
+        return
+    os.system('python run.py -t i -i %s --vlen %d --vsew %d --lmul %s --elen %d' % (ins, vlen, vsew, lmul_str, elen))
 
 def runcommand_fixpoint(ins):
-    os.system('python run.py -t x -i %s --vlen 256 --vsew 16 --lmul 8' % ins)
+    os.system('python run.py -t x -i %s --vlen %d --vsew %d --lmul %s --elen %d' % (ins, vlen, vsew, lmul_str, elen))
 
 def runcommand_permute(ins):
-    os.system('python run.py -t p -i %s --vlen 256 --vsew 16 --lmul 8' % ins)
-
+    if ins == "vfslide" and (vsew == 8 or vsew == 16):
+        return
+    if ins == "vrgatherei16" and ((vsew == 8 and lmul_str == "8") or (vsew == 32 and lmul_str == "0.125") or (vsew == 64 and lmul_str == "0.125") or (vsew == 64 and lmul_str == "0.25")):
+        return
+    os.system('python run.py -t p -i %s --vlen %d --vsew %d --lmul %s --elen %d' % (ins, vlen, vsew, lmul_str, elen))
 
 def runcommand_floatingpoint(ins):
-    os.system('python run.py -t f -i %s --vlen 256 --vsew 16 --lmul 8' % ins)
+    if (vsew == 8 or vsew == 64 or lmul_str == "0.125" or lmul_str == "8") and (ins.startswith('vfw') or ins.startswith('vfn')):
+        return
+    if (vsew == 8 or vsew == 16):
+        return
+    os.system('python run.py -t f -i %s --vlen %d --vsew %d --lmul %s --elen %d' % (ins, vlen, vsew, lmul_str, elen))
 
 def runcommand_loadstore(ins):
-    os.system('python run.py -t l -i %s --vlen 256 --vsew 16 --lmul 8' % ins)
+    os.system('python run.py -t l -i %s --vlen %d --vsew %d --lmul %s --elen %d' % (ins, vlen, vsew, lmul_str, elen))
 
 def runcommand_mask(ins):
-    # passed 32
-    os.system('python run.py -t m -i %s --vlen 256 --vsew 16 --lmul 8' % ins)
+    os.system('python run.py -t m -i %s --vlen %d --vsew %d --lmul %s --elen %d' % (ins, vlen, vsew, lmul_str, elen))
     
 
 def run_integer():
@@ -82,12 +90,19 @@ def run_loadstore():
     pool = multiprocessing.Pool(1)
     pool.map(runcommand_loadstore, loadstore)
 
+# Modify here to config
+vlen = 256
+vsew = 32
+lmul_str = "1"  # "1", "2", "4", "8", "0.25", "0.5", "0.125"
+elen = 64
+
 # Generate all and Put final ELF to a directory
 def main():
     subprocess.run(["mkdir", "-p", 'generate_all'])
     setup_logging(True)
     # Modify here to choose which categories you want to generate
     run_integer()
+    run_floatingpoint()
     run_mask()
     run_fixpoint()
     run_permute()
